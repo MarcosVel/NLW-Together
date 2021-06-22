2acc0c98eba75d9317fa28e43d1fc9fd59f2e257
MarcosVel <marcos.veloso@sempreceub.com>
2021-06-22 00:28:33 -0300

    state w/ text input

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+import { styles } from './styles';
+
+export function SignIn() {
+
+  const [ text, setText ] = useState('');
+
+  return (
+    <View style={ styles.container }>
+      <Text>Open up App.tsx to start working on your app!</Text>
+
+      <TextInput
+        style={ styles.input }
+        onChangeText={ setText }
+      />
+
+      <Text>
+        Você digitou: { text }
+      </Text>
+    </View>
+  );
+}
