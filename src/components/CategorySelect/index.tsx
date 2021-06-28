0f58b41983f05b9c76d6c2c5e8c32fb0896cd3ed
MarcosVel <marcos.veloso@sempreceub.com>
2021-06-27 22:55:16 -0300

    select category

--- src/components/CategorySelect/index.tsx
+++ src/components/CategorySelect/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 import { ScrollView } from 'react-native';
 
 import { styles } from './styles';
+import { categories } from '../../utils/categories';
+import { Category } from '../Category';
 
-export function CategorySelect() {
+type Props = {
+  categorySelected: string;
+  setCategory: (categoryId: string) => void;
+}
+
+export function CategorySelect({
+  categorySelected,
+  setCategory
+}: Props) {
   return (
     <ScrollView
       style={ styles.container }
@@ -11,7 +21,17 @@ export function CategorySelect() {
       showsHorizontalScrollIndicator={ false }
       contentContainerStyle={ { paddingRight: 40 } }
     >
-      
+      {
+        categories.map(category => (
+          <Category
+            key={ category.id }
+            title={ category.title }
+            icon={ category.icon }
+            checked={ category.id === categorySelected }
+            onPress={ () => setCategory(category.id) }
+          />
+        ))
+      }
     </ScrollView>
   );
 }
