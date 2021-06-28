45494e1165a42e1d8d159339e3fb1ad97a6e2815
MarcosVel <marcos.veloso@sempreceub.com>
2021-06-27 22:19:15 -0300

    add svgs and configure

---
+++ src/components/CategorySelect/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+
+import { styles } from './styles';
+
+export function CategorySelect() {
+  return (
+    <ScrollView
+      style={ styles.container }
+      horizontal
+      showsHorizontalScrollIndicator={ false }
+      contentContainerStyle={ { paddingRight: 40 } }
+    >
+      
+    </ScrollView>
+  );
+}
