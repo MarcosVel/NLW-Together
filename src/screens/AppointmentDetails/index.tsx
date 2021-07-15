efb374b7bdc153a756e65e1be3f354a4dbcb25da
MarcosVel <marcos.veloso@sempreceub.com>
2021-07-14 22:34:03 -0300

    add members in Details

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ImageBackground, Text, View } from 'react-native';
+import { ImageBackground, Text, View, FlatList } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { Fontisto } from '@expo/vector-icons';
 import { Background } from '../../components/Background';
@@ -8,8 +8,26 @@ import { Header } from '../../components/Header';
 import BannerImg from '../../assets/banner.png'
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
+import { ListHeader } from '../../components/ListHeader';
+import { Member } from '../../components/Member';
+import { ListDivider } from '../../components/ListDivider';
 
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: '1',
+      username: 'Marcos',
+      avatar_url: 'http://github.com/MarcosVel.png',
+      status: 'online'
+    },
+    {
+      id: '2',
+      username: 'Veloso',
+      avatar_url: 'http://github.com/MarcosVel.png',
+      status: 'offline'
+    }
+  ]
+
   return (
     <Background>
       <Header
@@ -28,11 +46,26 @@ export function AppointmentDetails() {
         source={ BannerImg }
         style={ styles.banner }
       >
-        <View style={ styles.bannerContent}>
+        <View style={ styles.bannerContent }>
           <Text style={ styles.title }>Lendários</Text>
           <Text style={ styles.subtitle }>É hoje que vamos chegar ao challenger sem perder uma partida da md10</Text>
         </View>
       </ImageBackground>
+
+      <ListHeader
+        title='Jogadores'
+        subtitle='Total 3'
+      />
+
+      <FlatList
+        data={ members }
+        keyExtractor={ item => item.id }
+        renderItem={ ({ item }) => (
+          <Member data={ item } />
+        ) }
+        ItemSeparatorComponent={ () => <ListDivider /> }
+        style={ styles.members }
+      />
 
     </Background>
   )
